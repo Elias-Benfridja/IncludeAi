338acdb58993682a0f4ce49014a9b8623a18f3c7
Elias <Elias.Benfridja06@gmail.com>
2026-08-06 22:41:24 +0100

    added related name to profile

--- backend/config/account/models.py
+++ backend/config/account/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     matching_enabled = models.BooleanField(default=False)
 
